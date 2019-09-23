3b171289ae2ce035e32fc0a7a767ec596ef80e13
Krisztian Holub <kholub1989@gmail.com>
2019-09-23 17:36:02 -0400

    OAuth 2.0 & How to Implement Sing in w Google and Facebook, letting User Submit Secret L6

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const express    = require('express'),
       mongoose   = require('mongoose'),
       session    = require('express-session'),
       passport   = require('passport'),
-      passportLocalMongoose = require('passport-local-mongoose');
+      passportLocalMongoose = require('passport-local-mongoose'),
+      GoogleStrategy = require('passport-google-oauth20').Strategy,
+      FacebookStrategy = require('passport-facebook').Strategy,
+      findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 const PORT = 3000;
@@ -35,20 +38,81 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
  
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.APP_ID,
+  clientSecret: process.env.APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log(profile);
+  User.findOrCreate({ facebookId: profile.id}, function(err, user) {
+    if (err) { return done(err); }
+    done(null, user);
+  });
+}
+));
 
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
+  });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook')
+);
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
 });
 
 app.get('/login', function(req, res){
@@ -60,11 +124,39 @@ app.get('/register', function(req, res){
 });
 
 app.get('/secrets', function(req, res){
+  User.find({'secret': {$ne: null}}, function(err, foundUsers){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render('secrets', {usersWithSecrets: foundUsers})
+      }
+    }
+  });
+});
+
+app.get('/submit', function(req, res){
   if (req.isAuthenticated()) {
-    res.render('secrets');
+    res.render('submit');
   } else {
     res.redirect('/login');
   }
+});
+
+app.post('/submit', function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect('/secrets');
+        });
+      }
+    }
+  })
 });
 
 app.get('/logout', function(req, res){
